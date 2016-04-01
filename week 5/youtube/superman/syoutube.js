f55348d962e9e88d3ecbcd3b9880c0eeee3d58d5
DaylanJones <dajon005@mail.goucher.edu>
2016-03-31 20:13:27 -0400

    Worked with API and Node

---
+++ week 5/youtube/superman/syoutube.js
@@ -0,0 +1,38 @@
+// wait for page to load
+$(window).load(function(){
+	var baseUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20';
+	var searchTerm = 'superman justic league'; // change this to the word/s you want to search for
+	var apiKey = "AIzaSyAoo1wqAKaLeQDZlUHzwsB6W3qT9y8yzP8"; 
+	var url = baseUrl + '&q=' + searchTerm + '&key=' + apiKey;
+
+    $.get(url, function (response) {
+    	// Check the web developer console for the whole result
+    	console.log(response);
+    	// all video links will start with this:
+    	var videoBaseUrl = 'https://www.youtube.com/watch?v=';
+    	// loop through response.items for videos
+    	for(var i = 0; i < response.items.length; i++){
+    		var videoId = response.items[i].id.videoId;
+    		var videoUrl = videoBaseUrl + videoId;
+
+    		// url to a still image representing the video
+    		var thumbnailImg = response.items[i].snippet.thumbnails.medium.url;
+
+    		// create link tag, <a>
+    		var link = $('<a href="' + videoUrl + '"></a>');
+
+    		// create image tag, <img>
+    		var img = $('<img src="' + thumbnailImg + '">');
+
+    		// attach image to link 
+    		link.append(img);
+
+    		// put the linked image into the container <div>
+    		//      -- see http://api.jquery.com/append/ for more info
+    		$('#container').append(link);
+
+    	}
+
+    }, "json"); //end $.get()
+
+}); // end window load listener
---
+++ week 5/youtubeAPI/youtube.js
@@ -0,0 +1,21 @@
+$(document).ready(){
+
+    var mybatBtn = document.getElementById("bat");
+    var mysupBtn = document.getElementById("sup");
+    var myothBtn = document.getElementById("oth");
+    
+    
+
+    mybatBtn.addEventListener("click",batbuttonAction);
+    mysupBtn.addEventListener("click",supbuttonAction);
+    myothBtn.addEventListener("click",othbuttonAction);
+
+// get the <ul> unordered list element
+var bat_man = document.getElementById("bat");
+var sup_man = document.getElementById("sup");
+var oth_man = document.getElementById("oth");
+
+//$('#bat').click(function(){
+    //$(this).attr("href"="http://madarts.goucher.edu/daylanjones/web-development-2016/week%205/batman/batman.html");
+  
+};
